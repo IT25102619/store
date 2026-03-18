ccce7c85a8eedc05b465298d7648a817fdefd8ee
IT25102619 <it25102619@my.sliit.lk>
2026-03-18 08:28:54 +0530

    print statement added

--- src/Exercise02/Main.java
+++ src/Exercise02/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Hello checking.");
+
         System.out.print("Enter the size of the circular queue : ");
         int size = scanner.nextInt();
 
